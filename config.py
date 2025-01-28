9ef70cf0e9c037c6f023c62295a03fbc18e78604
OrewaSanjii-Dono <sanjihunbechutiye+new56@gmail.com>
2025-01-28 08:33:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,8 +65,8 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 #Short Url or Api
-SHORT_URL = os.environ.get("SHORTNER_URL", "inshorturl.com")
-SHORT_API = os.environ.get("SHORTNER_API", "b83fdd727060c59a43b9b9b063dcee500190de3d")
+SHORT_URL = os.environ.get("SHORTNER_URL", "modijiurl.com")
+SHORT_API = os.environ.get("SHORTNER_API", "62401acaca5992bcc3b96645f310eca0b0cb607d")
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "Pʟᴇᴀꜱᴇ ᴅᴏɴ'ᴛ ᴍᴇꜱꜱᴀɢᴇ ᴍᴇ ᴅɪʀᴇᴄᴛʟʏ ɪ ᴀᴍ ᴏɴʟʏ ᴡᴏʀᴋ ꜰᴏʀ - [ @Adult_Flux ]"
